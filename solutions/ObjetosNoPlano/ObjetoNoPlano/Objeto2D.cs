142998eb3274dd2c590880864a367600218b2b7f
Rafael <rafaelcezarc@gmail.com>
2017-06-13 19:21:57 -0300

    Atualização da lista de exercicios completa

---
+++ solutions/ObjetosNoPlano/ObjetoNoPlano/Objeto2D.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ObjetoNoPlano
+{
+    class Objeto2D
+    {
+        public int x, y; 
+
+        public void AndarParaDireita()
+        { 
+            x = x + 1; 
+        }
+        public void AndarParaEsquerda()
+        {
+            x = x - 1; 
+        }
+        public void AndarParaCima()
+        {
+            y = y + 1; 
+        }
+        public void AndarParaBaixo()
+        {
+            y = y - 1;
+        }
+        public string Coordenadas()
+        {
+            return String.Format("{0}, {1}", x, y);
+        }
+
+    }
+
+}
