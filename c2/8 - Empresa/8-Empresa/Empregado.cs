4e8d64d9b7279545e56c52d0ad3602cc9d2ae904
Rafael <rafaelcezarc@gmail.com>
2017-09-22 21:12:01 -0300

    Exercício 8 com hierarquia

--- c2/8 - Empresa/8-Empresa/Empregado.cs
+++ c2/8 - Empresa/8-Empresa/Empregado.cs
@@ -6,43 +6,18 @@ using System.Threading.Tasks;
 
 namespace _8_Empresa
 {
-    class Empregado
+    class Empregado : Funcionario
     {
-        public string Nome { get; set; }
-
-        public double Salario { get; set; }
-
-        public string Email { get; set; } 
-        
-        private int nro_registro;
-
-        public int Nro_registro
-        {
-            get { return nro_registro; }
-        }
-
-        private int cpf;
-
-        public int CPF
-        {
-            get { return cpf; }
-        }
-
-        private int dta_entrada;
-
-        public int Dta_Entrada
+        private string dta_entrada;
+        public string Dta_Entrada 
         {
             get { return dta_entrada; }
         }
+        public Gerente Ger { get; set; }
 
-        public int ID_gerente { get; set; }
-
-        public Empregado(int cpf, int nro, int ger, int dta)
+        public Empregado (int reg, string dta) : base (reg)
         {
-            this.cpf = cpf;
-            nro_registro = nro;
-            ID_gerente = ger;
-            dta_entrada = dta;
-        }        
+            this.dta_entrada = dta;
+        }
     }
 } 
---
+++ c2/8 - Empresa/8-Empresa/Funcionario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Empresa
+{
+    class Funcionario
+    {
+        public Funcionario (int reg)
+        {
+            nro_registro = reg;
+        }
+
+        public string Nome { get; set; }
+
+        public double Salario { get; set; }
+
+        public string Email { get; set; }
+
+        private int nro_registro;
+        public int Nro_registro
+        {
+            get { return nro_registro; }
+        }
+
+        private string cpf;
+        public string CPF 
+        {
+            get { return cpf; } 
+        }
+    }
+}
